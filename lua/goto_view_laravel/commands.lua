632dcb061dd1aabe47db0879decbf5358dc3cb9e
yahyasghayron <yahya.sghayron123@gmail.com>
2024-12-23 20:47:40 +0100

    feat: change the default maping form ov to gv

--- lua/goto_view_laravel/commands.lua
+++ lua/goto_view_laravel/commands.lua
@@ -27,37 +27,37 @@ end
 
 -- Function to open a Blade view
 function M.open_blade_view()
-   -- Find the root directory
-   local root = find_project_root()
-   if not root then
-     print("Project root not found! Ensure you are in a Laravel project.")
-     return
-   end
- 
-   -- Get the full string under the cursor
-   local cursor_string = vim.fn.expand("<cWORD>")
-   -- Extract the Blade view path (e.g., 'pages.sante.index')
-   local blade_view = cursor_string:match("view%s*%(%s*['\"]([^'\"]+)['\"]")
-   if not blade_view then
-     print("No valid Blade view reference found under the cursor.")
-     return
-   end
- 
-   -- Replace dots with slashes to form the file path
-   local file_path = root .. "/resources/views/" .. blade_view:gsub("%.", "/") .. ".blade.php"
- 
-   -- Open the Blade file
-   if vim.fn.filereadable(file_path) == 1 then
-     vim.cmd("edit " .. file_path)
-   else
-     print("Blade file not found: " .. file_path)
-   end
+  -- Find the root directory
+  local root = find_project_root()
+  if not root then
+    print("Project root not found! Ensure you are in a Laravel project.")
+    return
+  end
+
+  -- Get the full string under the cursor
+  local cursor_string = vim.fn.expand("<cWORD>")
+  -- Extract the Blade view path (e.g., 'pages.sante.index')
+  local blade_view = cursor_string:match("view%s*%(%s*['\"]([^'\"]+)['\"]")
+  if not blade_view then
+    print("No valid Blade view reference found under the cursor.")
+    return
+  end
+
+  -- Replace dots with slashes to form the file path
+  local file_path = root .. "/resources/views/" .. blade_view:gsub("%.", "/") .. ".blade.php"
+
+  -- Open the Blade file
+  if vim.fn.filereadable(file_path) == 1 then
+    vim.cmd("edit " .. file_path)
+  else
+    print("Blade file not found: " .. file_path)
+  end
 end
 
 -- Set up keybinding for the command
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>ov",
+  "<leader>gv",
   ":lua require('goto_view_laravel.commands').open_blade_view()<CR>",
   { noremap = true, silent = true }
 )
